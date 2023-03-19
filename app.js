4df4b4ac04a1ce6bfdc1d3f3e83f2a866eb3a218
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-19 13:13:08 +0600

    button handleKitkatBtn()

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.getElementById('kitkat-buy-btn').addEventListener('click',function(){
+//     // console.log('btn clicked'); -worked before onclick added to the button
+// });
+function handleKitkatBtn(){
+    // console.log('hi');
+    // worked - alert to the id name-handleKitkatBtn()
+}
